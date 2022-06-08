1d741769f4da97f6a53c3e3f913accdef300506e
Pedro Miguel Ferreira Ribeiro <prs.p.ribeiro@gmail.com>
2022-06-08 21:01:21 +0100

    FixOneMinuteComand

--- Controllers/OpenSeaController.cs
+++ Controllers/OpenSeaController.cs
@@ -104,16 +104,16 @@ public class OpenSeaController : BaseApiController
 
     }
     [HttpGet, Route("x_minutesByMinutes")]
-    public List<teste> GetMinuteAddedSale(int Minutes)
+    public List<teste> GetMinuteAddedSale(int Minutes=1)
     {
+        Minutes = Minutes + 60;
         var x = _dataContext.db_AssetEvent
             .Where(x=>x.EventTimestamp <=  DateTime.Now & x.EventTimestamp >= DateTime.Now.AddMinutes(-Minutes))
-            .GroupBy(x => new { x.CollectionSlug, x.EventTimestamp.Minute })
+            .GroupBy(x => new { x.CollectionSlug})
             //.Join(_dataContext.db_Collection,)
             .Select(x => new teste
             {
                 CollectionSlug = x.Key.CollectionSlug,
-                Minute = x.Key.Minute,
                 mean_price = x.Average(a=> Convert.ToDouble(a.TotalPrice)),
                 total = x.Sum(a=>Convert.ToDouble(a.Quantity)),
                 NumResults = x.Count(),
