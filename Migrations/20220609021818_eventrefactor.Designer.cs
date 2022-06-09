fb27a708a9a2dc55b85c27a56cc2b2b699d7514b
Pedro Miguel Ferreira Ribeiro <prs.p.ribeiro@gmail.com>
2022-06-09 03:18:34 +0100

    migration

--- Migrations/20220609021818_eventrefactor.Designer.cs
+++ Migrations/20220609021818_eventrefactor.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OpenSeaWebApi.Data;
@@ -11,9 +12,10 @@ using OpenSeaWebApi.Data;
 namespace OpenSeaWebApi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220609021818_eventrefactor")]
+    partial class eventrefactor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20220609021818_eventrefactor.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenSeaWebApi.Migrations
+{
+    public partial class eventrefactor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnimationOriginalUrl",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "AnimationUrl",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "BackgroundColor",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "Decimals",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalLink",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "ImageOriginalUrl",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "ImagePreviewUrl",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "ImageThumbnailUrl",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "IsNsfw",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "NumSales",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "Permalink",
+                table: "Asset");
+
+            migrationBuilder.DropColumn(
+                name: "TokenMetadata",
+                table: "Asset");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AnimationOriginalUrl",
+                table: "Asset",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AnimationUrl",
+                table: "Asset",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BackgroundColor",
+                table: "Asset",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Decimals",
+                table: "Asset",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Asset",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalLink",
+                table: "Asset",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageOriginalUrl",
+                table: "Asset",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePreviewUrl",
+                table: "Asset",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageThumbnailUrl",
+                table: "Asset",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Asset",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsNsfw",
+                table: "Asset",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Asset",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumSales",
+                table: "Asset",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Permalink",
+                table: "Asset",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TokenMetadata",
+                table: "Asset",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
